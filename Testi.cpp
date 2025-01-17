01b6319a5e1aaee22bcc493bc87862d68d3dc0a5
Markus Koski <markuskoski298@gmail.com>
2025-01-17 10:35:59 +0200

    C testausta

--- Testi.cpp
+++ Testi.cpp
@@ -1,5 +1,27 @@
-//Markus Koski, Frans-Emil Vuori, Miika Musta
+#include <iostream>
 
-// muutos ja tämmä vikaa muutos testausta varten
+int main() {
 
-// Frans was here
+    const int maara = 5;
+
+    double lampo[maara];
+
+    double yhteensa = 0.0;
+
+    std::cout << "Anna 5 lämpöarvoa:\n";
+
+    for (int i = 0; i < maara; ++i) {
+
+        std::cout << "Lämpötila " << (i + 1) << ": ";
+
+        std::cin >> lampo[i];
+
+        yhteensa += lampo[i];
+    }
+
+    double keskiarvo = yhteensa / maara;
+
+    std::cout << "Keskiarvo: " << keskiarvo << std::endl;
+
+    return 0;
+}
