d366ba41ad13f4f46a2656f1f44f898c5cbcba39
miikamusta1 <miku.musta@gmail.com>
2025-01-24 09:21:16 +0200

    testi2

--- 2DRougeLike.py
+++ 2DRougeLike.py
@@ -3,50 +3,48 @@
 # Frans-Emil Vuori
 # TITE23
 
-
-
 import pygame
 import sys
 
-
-
-# pygame setup
+# pygame asetukset
 pygame.init()
-screen = pygame.display.set_mode((1280, 720))
-clock = pygame.time.Clock()
-running = True
+naytto = pygame.display.set_mode((1280, 720))
+kello = pygame.time.Clock()
+kaynnissa = True
 dt = 0
 
-player_pos = pygame.Vector2(screen.get_width() / 2, screen.get_height() / 2)
+pelaaja_sijainti = pygame.Vector2(naytto.get_width() / 2, naytto.get_height() / 2)
 
-while running:
-    # poll for events
-    # pygame.QUIT event means the user clicked X to close your window
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False
+while kaynnissa:
+    # tapahtumien tarkistaminen
+    # pygame.QUIT tapahtuma tarkoittaa, että käyttäjä napsautti X sulkeakseen ikkunan
+    for tapahtuma in pygame.event.get():
+        if tapahtuma.type == pygame.QUIT:
+            kaynnissa = False
 
-    # fill the screen with a color to wipe away anything from last frame
-    screen.fill("purple")
+    # täytä näyttö väreillä, jotta edellinen kuva poistuu
+    naytto.fill("purple")
 
-    pygame.draw.circle(screen, "red", player_pos, 40)
+    # piirrä ympyrä pelaajan sijaintiin
+    pygame.draw.circle(naytto, "red", pelaaja_sijainti, 40)
 
-    keys = pygame.key.get_pressed()
-    if keys[pygame.K_w]:
-        player_pos.y -= 300 * dt
-    if keys[pygame.K_s]:
-        player_pos.y += 300 * dt
-    if keys[pygame.K_a]:
-        player_pos.x -= 300 * dt
-    if keys[pygame.K_d]:
-        player_pos.x += 300 * dt
+    # tarkistetaan, onko näppäimiä painettu
+    napit = pygame.key.get_pressed()
+    if napit[pygame.K_w]:
+        pelaaja_sijainti.y -= 300 * dt
+    if napit[pygame.K_s]:
+        pelaaja_sijainti.y += 300 * dt
+    if napit[pygame.K_a]:
+        pelaaja_sijainti.x -= 300 * dt
+    if napit[pygame.K_d]:
+        pelaaja_sijainti.x += 300 * dt
 
-    # flip() the display to put your work on screen
+    # flip() näyttää päivitykset näytöllä
     pygame.display.flip()
 
-    # limits FPS to 60
-    # dt is delta time in seconds since last frame, used for framerate-
-    # independent physics.
-    dt = clock.tick(60) / 1000
+    # rajoittaa FPS:n 60:een
+    # dt on aikaväli edellisen ruudun ja nykyisen ruudun välillä sekunteina,
+    # jota käytetään ruudunpäivityksestä riippumattomassa fysiikassa.
+    dt = kello.tick(60) / 1000
 
 pygame.quit()
