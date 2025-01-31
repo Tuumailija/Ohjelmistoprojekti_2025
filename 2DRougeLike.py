1476835c8da68781abffa71ce797cb2f21fb4b5d
miikamusta1 <miku.musta@gmail.com>
2025-01-31 09:23:13 +0200

    seinäcollider testi

--- 2DRougeLike.py
+++ 2DRougeLike.py
@@ -19,45 +19,63 @@ dt = 0
 
 # Lataa taustakuva
 nykyinen_kansio = os.path.dirname(__file__)
-
-# Muodosta absoluuttinen polku kuvaan
 kuvapolku = os.path.join(nykyinen_kansio, "Metsa.png")
-
-# Lataa taustakuva
 tausta1 = pygame.image.load(kuvapolku)
 
 # Pelaajan sijainti (keskellä näyttöä)
 pelaaja_sijainti = pygame.Vector2(naytto.get_width() / 2, naytto.get_height() / 2)
+pelaaja_sade = 20  # Ympyrän säde
+
+# Seinälistan luonti (Rect-muodossa)
+seinät = [
+    pygame.Rect(200, 200, 200, 50),  # Esimerkkiseinä
+    pygame.Rect(500, 400, 300, 50),  # Toinen seinä
+]
+
+def tarkista_tormays(x, y):
+    """ Tarkistaa, osuisiko pelaaja seinään uudessa sijainnissa. """
+    pelaaja_rect = pygame.Rect(x - pelaaja_sade, y - pelaaja_sade, pelaaja_sade * 2, pelaaja_sade * 2)
+    for seinä in seinät:
+        if pelaaja_rect.colliderect(seinä):
+            return True  # Törmäys tapahtuu
+    return False  # Ei törmäystä
 
 while kaynnissa:
     # tapahtumien tarkistaminen
-    # pygame.QUIT tapahtuma tarkoittaa, että käyttäjä napsautti X sulkeakseen ikkunan
     for tapahtuma in pygame.event.get():
         if tapahtuma.type == pygame.QUIT:
             kaynnissa = False
 
-    # Piirrä taustakuva näyttöön
+    # Piirrä taustakuva
     naytto.blit(tausta1, (0, 0))
 
-    # Piirrä ympyrä pelaajan sijaintiin
-    pygame.draw.circle(naytto, "red", pelaaja_sijainti, 40)
+    # Piirrä seinät
+    for seinä in seinät:
+        pygame.draw.rect(naytto, "blue", seinä)
 
-    # Tarkistetaan, onko näppäimiä painettu
+    # Piirrä pelaaja
+    pygame.draw.circle(naytto, "red", pelaaja_sijainti, pelaaja_sade)
+
+    # Pelaajan liikkuminen
     napit = pygame.key.get_pressed()
+    uusi_x, uusi_y = pelaaja_sijainti.x, pelaaja_sijainti.y
+
     if napit[pygame.K_w]:
-        pelaaja_sijainti.y -= 300 * dt
+        uusi_y -= 300 * dt
     if napit[pygame.K_s]:
-        pelaaja_sijainti.y += 300 * dt
+        uusi_y += 300 * dt
     if napit[pygame.K_a]:
-        pelaaja_sijainti.x -= 300 * dt
+        uusi_x -= 300 * dt
     if napit[pygame.K_d]:
-        pelaaja_sijainti.x += 300 * dt
+        uusi_x += 300 * dt
+
+    # Tarkista törmäys ennen päivittämistä
+    if not tarkista_tormays(uusi_x, uusi_y):
+        pelaaja_sijainti.x = uusi_x
+        pelaaja_sijainti.y = uusi_y
 
     # Päivitä näyttö
     pygame.display.flip()
-
-    # Rajoita FPS:ää 60:een
-    # dt on aikaväli edellisen ja nykyisen ruudun välillä sekunteina, jota käytetään fysiikassa.
     dt = kello.tick(60) / 1000
 
 pygame.quit()
